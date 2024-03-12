e870fa20f12f94e21340f8f114fa84d7e96ec4dd
s28305 <s28305@pjwstk.edu.pl>
2024-03-12 12:30:05 +0100

    CalcAverage modification1

--- APBD1/Program.cs
+++ APBD1/Program.cs
@@ -1,3 +1,11 @@
 ﻿int num1 = 33;
 int num2 = num1*5 - 30;
 Console.WriteLine("Number: " + num2);
+
+int[] numbers = {10, 15, 7, 2, 77};
+double average = CalcAverage(numbers);
+Console.WriteLine("Average: " + average);
+
+static double CalcAverage(int[] numbers) {
+   return 0;
+}
