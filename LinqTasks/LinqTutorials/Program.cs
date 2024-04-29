f159f85c2cf5060ab544f468fff34213988fa146
s28305 <s28305@pjwstk.edu.pl>
2024-04-30 00:57:02 +0300

    Commit1

---
+++ LinqTasks/LinqTutorials/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task7();
+            foreach (var a in t)
+            {
+                Console.WriteLine(a);
+            }
+
+
+        }
+    }
+}
