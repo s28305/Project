4ee65f5333157e71b168cb4cf181216469281456
s28305 <161834661+s28305@users.noreply.github.com>
2024-06-28 14:08:58 +0200

    Delete Tutorial5 directory

--- Tutorial5/Models/Visit.cs
+++
@@ -1,9 +0,0 @@
-namespace Tutorial5.Models;
-
-public class Visit(DateTime date, string description, double price, Animal animal)
-{
-    public DateTime Date { get; set; } = date;
-    public string Description { get; set; } = description;
-    public double Price { get; set; } = price;
-    public Animal Animal { get; set; } = animal;
-}
